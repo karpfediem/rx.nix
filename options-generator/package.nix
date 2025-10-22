9b3af1c419e11612350db1817aa94c700fe6015e
Karpfen <karpfen@karpfen.dev>
2025-10-22 09:25:49 +0200

    nix: Define subPackages for options-generator package

--- options-generator/package.nix
+++ options-generator/package.nix
@@ -3,5 +3,6 @@ buildGoModule {
   pname = "options-generator";
   version = "0.1.0";
   src = lib.cleanSource ./.;
+  subPackages = [ "." ];
   vendorHash = "sha256-SlG9Qd7NGlCJKgDY0+U8muuuGEQ7KFMQZdeb5gl+Xts=";
 }
